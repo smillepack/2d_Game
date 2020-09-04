b36bec1276da044a43474b366d4eff4fe6ab9ce7
mnogobarsukov <alexander0krivonosov#gmail.com>
2020-09-04 19:14:21 +0300

    first-commit

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const WALL_WIDTH  = 5
 let keyPressUp = new KeyPressUp()
 let player = new Player(GAME_HEIGHT, GAME_WIDHT, ctx, keyPressUp, WALL_WIDTH)
 let map    = new Map(GAME_HEIGHT, GAME_WIDHT, ctx, WALL_WIDTH)
-
+// hi from another pc
 
 function gameLoop() {
     ctx.clearRect(0, 0, GAME_WIDHT, GAME_HEIGHT)
@@ -25,3 +25,4 @@ function gameLoop() {
 
 gameLoop()
 
+console.log('hi from new laptop')
